431e3f92318d92f79a5eb6ba2eaee70776f04ac2
любовь <lubov0strelnikova@gmail.com>
2023-05-28 22:18:02 +0300

    add parametrize

--- ui/tests/test_rooms.py
+++ ui/tests/test_rooms.py
@@ -1,16 +1,14 @@
 from faker import Faker
+import pytest
 
 from ui.data.model import Room
 from ui.pages.admin_rooms_page import AdminRooms
 
 fake = Faker()
 
-room = Room(name='test_room', price="100", type="Double", accessible="True")
-room_update = Room(name='room', price="100", type="Single", accessible="True")
-room_updated = Room(name='new_room', price="500", type="Twin", accessible="False")
-
-
-def test_create_room(open_browser_with_cookie):
+@pytest.mark.parametrize("type", ["Single", "Double", "Twin", "Family", "Suite"])
+def test_create_room(open_browser_with_cookie, type):
+    room = Room(name='test_room', price="100", type=type, accessible="True")
     list_rooms = AdminRooms()
     list_rooms.create_room(room)
 
@@ -22,6 +20,10 @@ def test_create_room(open_browser_with_cookie):
 
 
 def test_update_room(open_browser_with_cookie):
+
+    room_update = Room(name='room', price="100", type="Single", accessible="True")
+    room_updated = Room(name='new_room', price="500", type="Twin", accessible="False")
+
     list_rooms = AdminRooms()
 
     list_rooms.create_room(room_update)
